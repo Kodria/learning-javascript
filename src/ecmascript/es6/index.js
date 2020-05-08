81dd491d355dbd3243c64b00de991407b6bcca7c
Nicolas Guerrero <nicolasf1402@gmail.com>
2020-05-08 16:10:19 -0500

    Fix bugs es6

--- src/ecmascript/es6/index.js
+++ src/ecmascript/es6/index.js
@@ -1,5 +1,6 @@
 //-------------------- Parametros por defecto -------------------------- //
 
+console.log('------------- Parametros por defecto -------------');
 // Version anterior - es5
 function newFunction(name, age, country) {
   var name = name || 'Nicolas'
@@ -17,8 +18,11 @@ function newFunction2(name = 'Nicolas', age = 28, country = 'Colombia') {
 newFunction2()
 newFunction2('Camilo')
 
+console.log('------------- END Parametros por defecto -------------');
+
 //--------------------- Template literals ------------------------- //
 
+console.log('------------- Template literals -------------');
 // Version anterior - es5
 
 let hello = "Hello"
@@ -29,9 +33,11 @@ console.log(epicPhrase)
 // Template literals
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2)
+console.log('------------- END Template literals -------------');
 
 //--------------------- Multilinea ------------------------- //
 
+console.log('------------- Multilinea -------------');
 // Version anterior - es5
 
 let lorem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. \n"
@@ -45,9 +51,11 @@ Odio atque fugit aperiam quo totam possimus velit, quos similique praesentium vi
 `
 
 console.log(lorem2)
+console.log('------------- END Multilinea -------------');
 
 //--------------------- Destructuración de elementos ------------------------- //
 
+console.log('------------- Destructuración de elementos -------------');
 let person = {
   name: 'Nicolas',
   age: 28,
@@ -61,9 +69,11 @@ console.log(person.name, person.age, person.country)
 
 let { name, age, country } = person
 console.log(name, age, country)
+console.log('------------- END Destructuración de elementos -------------');
 
 //--------------------- Operador de propagación (Spread operator) ------------------------- //
 
+console.log('------------- Operador de propagación (Spread operator) -------------');
 let team1 = ['Usuario 1', 'Usuario 2', 'Usuario 3']
 let team2 = ['Usuario 4', 'Usuario 5', 'Usuario 6']
 
@@ -73,9 +83,11 @@ let team2 = ['Usuario 4', 'Usuario 5', 'Usuario 6']
 // es6
 let education = ['Usuario 7', ...team1, ...team2]
 console.log(education)
+console.log('------------- END Operador de propagación (Spread operator) -------------');
 
 //--------------------- Asignación en memoria ------------------------- //
 
+console.log('------------- Asignación en memoria -------------');
 var helloVar = 'Hello var' // Esta disponible en toda la aplicación
 let hellolet = 'Hello let' // Esta disponible solo en el scope (Bloque de codigo donde se invoca)
 
@@ -89,30 +101,45 @@ let hellolet = 'Hello let' // Esta disponible solo en el scope (Bloque de codigo
 }
 
 console.log(globalVar)
-console.log(globalLet) // Error
+try {
+  console.log(globalLet) // Error
+} catch(e) {
+  console.log('Se ha producido un error al imprimir globalLet');
+  console.log(e)
+}
 
 const a = 'b'
-a = 'a'
+try {
+  a = 'a'
+} catch(e) {
+  console.log('Se ha producido un error al reasignar una constante')
+}
 console.log(a)
+console.log('------------- END Asignación en memoria -------------');
+
 
 //--------------------- Propiedad de objetos mejorada ------------------------- //
 
-let name = 'Nicolas'
-let age = 28
+console.log('------------- Propiedad de objetos mejorada -------------');
+let _name = 'Nicolas'
+let _age = 28
 
 // Version anterior - es5
 
 let obj = {
-  name: name, age: age
+  _name: _name, _age: _age
 }
 
 // es6
-let obj2 = {name, age}
+let obj2 = {_name, _age}
 
 console.log(obj, obj2)
+console.log('------------- END Propiedad de objetos mejorada -------------');
+
 
 //--------------------- Arrow function ------------------------- //
 
+console.log('------------- Arrow function -------------');
 const names = [
   {name: 'Nicolas', age: 28},
   {name: 'Camilo', age: 27}
@@ -123,9 +150,12 @@ let listOfNames = names.map(function(item) {
 })
 
 let listOfNames2 = names.map(item => console.log(item.name))
+console.log('------------- END Arrow function -------------');
+
 
 //--------------------- Promise ------------------------- //
 
+console.log('------------- Promise -------------');
 const helloPromise = (status) => {
   return new Promise((resolve, reject) => {
     if (status === true) resolve('Hey!')
@@ -137,10 +167,13 @@ const helloPromise = (status) => {
 helloPromise(true)
   .then(result => console.log(result))
   .catch(err => console.error(err))
+console.log('------------- END Promise -------------');
+
 
 
 //--------------------- Classes ------------------------- //
 
+console.log('------------- Classes -------------');
 class Calculator {
   constructor() {
     this.valueA = 0;
@@ -157,15 +190,21 @@ class Calculator {
 const calculator = new Calculator();
 let sum = calculator.sum(1,2)
 console.log(sum)
+console.log('------------- END Classes -------------');
+
 
 //--------------------- Modulos ------------------------- //
 
-import moduleE6 from './module'
+console.log('------------- Modulos -------------');
+// import moduleE6 from './module'
 
-moduleE6()
+// moduleE6()
+console.log('------------- END Modulos -------------');
+
 
 //--------------------- Generator ------------------------- //
 
+console.log('------------- Generator -------------');
 function* helloWorld() {
   if (true) {
     yield 'Hello, '
@@ -180,3 +219,4 @@ const generatorHello = helloWorld()
 console.log(generatorHello.next().value)
 console.log(generatorHello.next().value)
 console.log(generatorHello.next().value)
+console.log('------------- END Generator -------------');
